3b1e262ffdc76b593631f0bc066e05b28b277b17
CHARLES <charlescallaghan@gmail.com>
2019-02-18 20:33:30 +0100

    Cleaned the move functionality up

--- client/src/components/PlayerOneContainer.js
+++ client/src/components/PlayerOneContainer.js
@@ -10,20 +10,6 @@ const keys = [],
     boardCenterX = WIDTH / 2,
     puckSize = 50;
 
-// let controllerOne = <Circle
-//     x={WIDTH / 5}
-//     y={HEIGHT / 2}
-//     radius={puckSize}
-//     fill={'green'}
-//     stroke={'black'}
-//     strokeWidth={3}
-//     mass={15}
-//     velocityX={0}
-//     velocityY={0}
-//     frictionX={0.997}
-//     frictionY={0.997}
-//     acceleration={1} />
-
 export default class PlayerOneContainer extends PureComponent {
     state = {
         positionX: WIDTH / 5,
@@ -65,43 +51,49 @@ export default class PlayerOneContainer extends PureComponent {
 
         keys[event.keyCode] = true
 
+
+        const move = () => {
+
+            // // Apply friction
+            this.setState({velocityX: this.state.velocityX * this.state.frictionX})
+            this.setState({velocityY: this.state.velocityY * this.state.frictionY})
+
+            // Update position
+            this.setState({positionX: this.state.positionX + this.state.velocityX})
+            this.setState({positionY: this.state.positionY + this.state.velocityY})
+        }
+
         // Up
         // if (keys[38] && controllerOne.velocityY < controllerOne.maxSpeed) {
         if (keys[38]) {
             this.setState({ velocityY: this.state.velocityY - this.state.acceleration })
+            move()
         }
 
         // // Down
         // if (keys[40] && controllerOne.velocityY < controllerOne.maxSpeed) {
         if (keys[40]) {
             this.setState({ velocityY: this.state.velocityY + this.state.acceleration })
+            move()
         }
 
         // Right
         // if (keys[39] && controllerOne.velocityX < controllerOne.maxSpeed) {
         if (keys[39]) {
             this.setState({ velocityX: this.state.velocityX + this.state.acceleration })
+            move()
         }
 
         // Left, decrease acceleration
         if (keys[37]) {
             // console.log(this.state.velocityX)
             this.setState({ velocityX: this.state.velocityX - this.state.acceleration })
+            move()
             // console.log(this.state.controllerOne.velocityX)
         }
 
     }
 
-    move = () => {
-
-        // // Apply friction
-        // this.velocityX *= this.frictionX;
-        // this.velocityY *= this.frictionY;
-
-        // Update position
-        this.state.positionX += this.state.velocityX;
-        this.state.positionY += this.state.velocityY;
-    }
 
     keysReleased = (event) => {
         keys[event.keyCode] = false;
@@ -109,7 +101,6 @@ export default class PlayerOneContainer extends PureComponent {
     // used to be ComponentWillMount
     componentDidMount() {
         window.addEventListener('keydown', this.moveController)
-        // window.addEventListener('keydown', this.move)
         window.addEventListener('keyup', this.keysReleased)
     }
 
@@ -140,8 +131,8 @@ export default class PlayerOneContainer extends PureComponent {
 
     componentDidUpdate() {
         this.keepPlayerInsideField()
-        this.move()
     }
+
     render() {
         return (
             <Circle
@@ -156,7 +147,8 @@ export default class PlayerOneContainer extends PureComponent {
                 velocityY={this.state.velocityY}
                 frictionX={this.state.frictionX}
                 frictionY={this.state.frictionY}
-                acceleration={this.state.acceleration} />
+                acceleration={this.state.acceleration}
+            />
         );
     }
 
