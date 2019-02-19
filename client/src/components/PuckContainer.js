db2ae69d5609c6d0927b31bced4da1c4f8c1d71b
Teun van Boxtel <teunvboxtel@hotmail.nl>
2019-02-19 14:05:11 +0100

    Puck now has a move function and is animated

--- client/src/components/PuckContainer.js
+++ client/src/components/PuckContainer.js
@@ -2,37 +2,50 @@ import React, { PureComponent } from "react";
 import { Circle } from "react-konva";
 import { WIDTH, HEIGHT } from "./PlayingFieldContainer";
 
-const MIN_X = 32,
-    MIN_Y = 32,
-    SPEED = 29;
+const puckSize = 25,
+    initialX = WIDTH / 2,
+    initialY = HEIGHT / 2
+
 
 export default class Puck extends PureComponent {
     state = {
-        color: 'black',
-        x: MIN_X,
-        y: MIN_Y,
-        direction: { x: 0, y: 0 }
+        positionX: initialX,
+        positionY: initialY,
+        mass: 15,
+        velocityX: 0,
+        velocityY: 0,
+        frictionX: 1,
+        frictionY: 1,
+        acceleration: 1
     };
 
+    move = () => {
+
+        this.setState({ velocityX: this.state.velocityX * this.state.frictionX })
+        this.setState({ velocityY: this.state.velocityY * this.state.frictionY })
+
+        this.setState({ positionX: this.state.positionX + this.state.velocityX })
+        this.setState({ positionY: this.state.positionY + this.state.velocityY })
+    }
+
     componentDidMount() {
-        const x = Math.floor(Math.random() * SPEED);
-        const y = SPEED - x;
-        this.setState({ direction: { x, y } });
+        this.animate()
+    }
+
+    animate = () => {
+        requestAnimationFrame(this.animate)
+        this.move()
     }
 
     render() {
-        const { color } = this.state;
-
         return (
             <Circle
-                ref={comp => {
-                    this.ball = comp;
-                }}
-                x={WIDTH / 2}
-                y={HEIGHT / 2}
-                radius={30}
-                fill={color}
-                shadowBlur={10}
+                x={initialX}
+                y={initialY}
+                radius={puckSize}
+                fill={'grey'}
+                stroke={'black'}
+                strokeWidth={2}
             />
         );
     }
