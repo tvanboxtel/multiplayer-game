eebf5995668467877c459944fb283fe0ff2758d5
CHARLES <charlescallaghan@gmail.com>
2019-02-19 12:24:42 +0100

    Setup basic backend server

---
+++ server/src/index.ts
@@ -0,0 +1,21 @@
+import 'reflect-metadata'
+import {createKoaServer} from "routing-controllers"
+// import setupDb from './db'
+
+const port = process.env.PORT || 4000
+
+const app = createKoaServer({
+    cors: true,
+    controllers: [
+        
+      ],
+ })
+ 
+ app.listen(port, () => console.log(`Listening on port ${port}`))
+
+// when DB is ready:
+//  setupDb()
+//  .then(_ =>
+//    app.listen(port, () => console.log(`Listening on port ${port}`))
+//  )
+//  .catch(err => console.error(err)) 
