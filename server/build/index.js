20c203b948892f7cefd40f8c3e70826fdae6c9dd
CHARLES <charlescallaghan@gmail.com>
2019-02-19 13:38:48 +0100

    Set up working test database

--- server/build/index.js
+++ server/build/index.js
@@ -2,10 +2,13 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 require("reflect-metadata");
 const routing_controllers_1 = require("routing-controllers");
+const db_1 = require("./db");
 const port = process.env.PORT || 4000;
 const app = routing_controllers_1.createKoaServer({
     cors: true,
     controllers: [],
 });
-app.listen(port, () => console.log(`Listening on port ${port}`));
+db_1.default()
+    .then(_ => app.listen(port, () => console.log(`Listening on port ${port}`)))
+    .catch(err => console.error(err));
 //# sourceMappingURL=index.js.map
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,6 @@
 import 'reflect-metadata'
 import {createKoaServer} from "routing-controllers"
-// import setupDb from './db'
+import setupDb from './db'
 
 const port = process.env.PORT || 4000
 
@@ -11,11 +11,10 @@ const app = createKoaServer({
       ],
  })
  
- app.listen(port, () => console.log(`Listening on port ${port}`))
 
 // when DB is ready:
-//  setupDb()
-//  .then(_ =>
-//    app.listen(port, () => console.log(`Listening on port ${port}`))
-//  )
-//  .catch(err => console.error(err)) 
+ setupDb()
+ .then(_ =>
+   app.listen(port, () => console.log(`Listening on port ${port}`))
+ )
+ .catch(err => console.error(err)) 
