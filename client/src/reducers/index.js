d116e6d61cacf112bc89cbdaf4f138b26c756fa5
Teun van Boxtel <teunvboxtel@hotmail.nl>
2019-02-21 16:45:28 +0100

    Front end side of player 2 should work

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,10 +2,8 @@ import { combineReducers } from 'redux'
 import playerOne from './playerOne'
 import playerTwo from './playerTwo'
 import puck from './puck'
-import player1 from './player1'
 
 export default combineReducers ({
-    player1,
     playerOne,
     playerTwo,
     puck
